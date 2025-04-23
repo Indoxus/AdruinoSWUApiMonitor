e815a9e4811acdc2f0ebe83f94f912a04b5d1612
= <=>
2025-04-23 13:00:13 +0200

    comments and readme

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,6 @@ int myFunction(int, int);
 String parsetimeleft(int t);
 String tolength(String str, int l);
 
-// set the LCD number of columns and rows
-int lcdColumns = 16;
-int lcdRows = 4;
-
 // WLAN creds
 const char *ssid = "";
 const char *pwd = "";
@@ -55,7 +51,7 @@ String parsetimeleft(int t) //formats the Seconds till Departure
   int seconds;
   z = (t<0)? "-":" ";
   t *= (t<0)? -1:1;
-  minutes = t / 60;
+  minutes = t / 60; // / is an integer divide
   seconds = t % 60;
   time += (minutes < 10)?"0" + String(minutes):String(minutes);
   time += ":";
@@ -63,7 +59,7 @@ String parsetimeleft(int t) //formats the Seconds till Departure
   return time;
 }
 
-String tolength(String str, int l)
+String tolength(String str, int l) // shortens or fills Strings
 {
   int lone = str.length();
   if (lone >= l)
@@ -81,44 +77,43 @@ String tolength(String str, int l)
 }
 
 
+HTTPClient http;
+int httpResponseCode;
+JsonDocument json;
 void loop()
 {
-  JsonDocument json;
+
   if (WiFi.status() == WL_CONNECTED) //keine Ahnug was passiert wenn man aus dem WLAN rein und rausläuft
   {
-    HTTPClient http;
+
     // API Req
     http.begin(Req.c_str());
 
     // Send HTTP GET request
-    int httpResponseCode = http.GET();
+    httpResponseCode = http.GET(); //currently don't undersand this library and trying to filter out "null"
     delay(500);
     if (httpResponseCode > 0)
     {
       const char *payload = http.getString().c_str();
       deserializeJson(json, payload);
-      if (json == NULL)
-      {
-        return;
-      }
+      if (json == NULL){return;}
       String time = json["StopPassage"]["CurrentTimestamp"];
-      lcd.clear();
+      if (time ==NULL){return;}
       lcd.setCursor(0, 0);
-      lcd.print(time.substring(11, 19));
+      lcd.print(time.substring(11, 19));// gets the time stamp we need
       lcd.print(" ");
       String stop = json["StopPassage"]["StopName"];
-      lcd.print(stop);
+      lcd.print(stop); // Auto cuts off as the next code writes over it, i was to lazy to count
       for (int i = 0; i < 3; i++)
       {
         String stopname = json["StopPassage"]["DepartureData"][i]["DepartureDirectionText"];
-        if (stopname != NULL)
-        {
-          lcd.setCursor(0, i + 1);
-          lcd.print(tolength(stopname, 14));
-          lcd.print(" ");
-          int t = json["StopPassage"]["DepartureData"][i]["DepartureCountdown"];
-          lcd.print(parsetimeleft(t));
-        }
+        if (stopname == NULL){return;} // escapes if req is corrupted 
+        lcd.setCursor(0, i + 1);
+        lcd.print(tolength(stopname, 14));
+        lcd.print(" ");
+        int t = json["StopPassage"]["DepartureData"][i]["DepartureCountdown"];
+        if (t == NULL){return;}
+        lcd.print(parsetimeleft(t));
       }
     }
     else
--- src/test.py
+++ src/test.py
@@ -10,4 +10,4 @@ n = 3
 r = requests.get('https://api.swu.de/mobility/v1/stop/passage/Departures?StopNumber='+str(stop)+'&Limit='+str(n))
 print(r._content)
 
-r2 = requests.get('https://api.swu.de/mobility/v1/route/attributes/BaseData?ContentScope=extended&RouteNumber=5')
+r2 = requests.get('https://api.swu.de/mobility/v1/route/attributes/BaseData?ContentScope=extended&RouteNumber=5') #change the last number to your route maybe just copy the link and open in firefox as it structures the json file
